29babec0d9362fc618e159e9fe45235d581853b2
nkkalmyk <suvv.mv@gmail.com>
2026-09-17 12:25:29 +0300

    7.09.26

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        long[] z = new long[8];
+
+        for (int i = 0; i < z.length; i++) {
+
+            z[i] = 21-2 * i;
+
+
+        }
+        System.out.println(Arrays.toString(z));
+
+        float[] x = new float[8];
+
+        float min = -11.0f;
+        float max = 3.0f;
+
+        for (int i = 0; i < x.length; i++) {
+            x[i] = (float) (Math.random() * (max - min) + min);
+        }
+        System.out.println(Arrays.toString(x));
+
+
 
 
     }
