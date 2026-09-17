410c883104dcf33a9cb7ec6941e85f12a28965e7
nkkalmyk <suvv.mv@gmail.com>
2026-09-17 14:02:56 +0300

    7.09.26

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
+
 
         long[] z = new long[8];
         for (int i = 0; i < z.length; i++) {
             z[i] = 21 - 2 * i;
         }
-        System.out.println(Arrays.toString(z));
 
 
         float[] x = new float[16];
@@ -16,28 +14,35 @@ public class Main {
         for (int i = 0; i < x.length; i++) {
             x[i] = (float) (Math.random() * (max - min) + min);
         }
-        System.out.println(Arrays.toString(x));
 
         double[][] n = new double[8][16];
         for (int i = 0; i < n.length; i++) {
             for (int j = 0; j < n[i].length; j++) {
 
-                float currentX = x[j];
-
-                if (z[i] == 17) {
-                    n[i][j] = Math.atan(Math.cos(Math.pow(Math.tan(currentX), Math.log(Math.abs(currentX)) + 1)));
-                } else if (z[i] == 9 || z[i] == 11 || z[i] == 13 || z[i] == 15) {
-                    n[i][j] = Math.exp(Math.pow(Math.tan(currentX) / (0.5 + Math.tan(currentX)), 2));
-                } else {
-
-                    n[i][j] = 4 * Math.cbrt(Math.tan(Math.pow(currentX, 2.0 / (3.0 * currentX))));
-                }
+                n[i][j] = calculateElement(z[i], x[j]);
             }
         }
 
 
-        for (double[] row : n) {
+        printMatrix(n);
+    }
+
+
+    public static double calculateElement(long zValue, float xValue) {
+        if (zValue == 17) {
+            return Math.atan(Math.cos(Math.pow(Math.tan(xValue), Math.log(Math.abs(xValue)) + 1)));
+        } else if (zValue == 9 || zValue == 11 || zValue == 13 || zValue == 15) {
+            return Math.exp(Math.pow(Math.tan(xValue) / (0.5 + Math.tan(xValue)), 2));
+        } else {
+            return 4 * Math.cbrt(Math.tan(Math.pow(xValue, 2.0 / (3.0 * xValue))));
+        }
+    }
+
+
+    public static void printMatrix(double[][] matrix) {
+        for (double[] row : matrix) {
             for (double v : row) {
+
                 System.out.printf("%8.3f ", v);
             }
             System.out.println();
